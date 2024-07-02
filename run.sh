6d069bc9c83321e98f6ac76d85a13a0cdc774268
ugwd01bFwiza <84223284+ugwd01bFwiza@users.noreply.github.com>
2024-07-02 14:24:04 +0800

    Modify run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,8 @@ year=$(date +%Y)
 month=$(date +%m)
 day=$(date +%d)
 
-clash="https://clashgithub.github.io/uploads/$year/$month/0-$year$month$day.yaml"
+clashgithub="https://clashgithub.github.io/uploads/$year/$month/0-$year$month$day.yaml"
 
-wget $clash -O ./clash.yaml
+wget $clashgithub -O ./clashgithub.yaml
+
+cp ./clashgithub.yaml ./clash.yaml
