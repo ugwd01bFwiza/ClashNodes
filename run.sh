b0b36053b681040af0a832419f3ab336d6eadb7c
ugwd01bFwiza <84223284+ugwd01bFwiza@users.noreply.github.com>
2024-07-02 14:09:28 +0800

    Fix url

--- run.sh
+++ run.sh
@@ -2,7 +2,6 @@ year=$(date +%Y)
 month=$(date +%m)
 day=$(date +%d)
 
-#clash="https://clashgithub.github.io/uploads/$year/$month/0-$year$month$day.yaml"
-clash="https://clashgithub.github.io/uploads/$year/06/0-${year}0627.yaml"
+clash="https://clashgithub.github.io/uploads/$year/$month/0-$year$month$day.yaml"
 
 wget $clash -O ./clash.yaml
