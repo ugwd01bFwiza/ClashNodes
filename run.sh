1414de84f2175f7d0df19ed4bbf23187fc002c9f
ugwd01bFwiza <84223284+ugwd01bFwiza@users.noreply.github.com>
2024-09-09 09:02:07 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@ year=$(date +%Y)
 month=$(date +%m)
 day=$(date +%d)
 
-#site=clashgithub
-site=myclash
+site=clashgithub
+#site=myclash
 
 url="https://$site.github.io/uploads/$year/$month/0-$year$month$day.yaml"
 
